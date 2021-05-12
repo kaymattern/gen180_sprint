b533a169965964d186fbd2b951139704a4e4d337
annagrace-hc <anna.grace@hackcville.com>
2021-05-12 11:14:41 -0400

    updated tucson viz

--- tucson-savings/tucson-savings.js
+++ tucson-savings/tucson-savings.js
@@ -1,9 +1,9 @@
 // Tucson Savings from Solar visualization
 
-const margin = {top:80, right:50, left:120, bottom:80}; 
+const margin = {top:50, right:50, left:50, bottom:50}; 
     
-const outerHeight = 800;   // can change dimensions
-const outerWidth = 1000;
+const outerHeight = 600;   // can change dimensions
+const outerWidth = 800;
    
 // total savings for one year: $1.1 million
 let tucsonData = [{type: "Teacher Salary", amount: 45000, quantity: 24},
@@ -29,10 +29,61 @@ const innerSolar = largeSolar.append("g");
 innerSolar.attr("transform", "translate(" +margin.left+","+margin.top+")")
 ; 
 
+
+salaries = innerSolar.append("g")
+buses = innerSolar.append("g")
+textbooks = innerSolar.append("g")
+laptops = innerSolar.append("g")
+supplies = innerSolar.append("g")
+
+var title = innerSolar.append("text")
+                        .text("Tuscon Savings")
+                         .style("text-anchor", "middle")
+                        .attr("transform", `translate(${innerWidth/2}, ${margin.top/8})`)
+                        .attr("dy", "1em")
+                        .attr("class", "tucsonTitle")
+                        .style("fill", "#F3F1A5")
+                        .attr('font-weight', 800)
+                        .attr('font-size', 32)
+                        .attr('font-family', 'Verdana');
+
+let increment = innerWidth/5;
+                
+salaries.attr("transform", "translate(" + 0*increment + "," + innerHeight/6 + ")");
+buses.attr("transform", "translate(" + 1*increment + "," + innerHeight/6 + ")");
+textbooks.attr("transform", "translate(" + 2*increment + "," + innerHeight/6 + ")");
+laptops.attr("transform", "translate(" + 3*increment + "," + innerHeight/6 + ")");
+supplies.attr("transform", "translate(" + 4*increment + "," + innerHeight/6 + ")");
+
+var suppliesImg = supplies.append("svg:image")
+   .attr("xlink:href", "https://cdn.onlinewebfonts.com/svg/img_532723.png" )
+   .attr("width", 80)
+   .attr("height", 80)
+
+var salImg = salaries.append("svg:image")
+   .attr("xlink:href", "http://cdn.onlinewebfonts.com/svg/img_353628.png")
+   .attr("width", 80)
+   .attr("height", 80)
+
+var busImg = buses.append("svg:image")
+   .attr("xlink:href", "https://cdn0.iconfinder.com/data/icons/transportation-and-logistics/50/Transportation_and_Logistics-67-512.png")
+   .attr("width", 80)
+   .attr("height", 80)
+
+var booksImg = textbooks.append("svg:image")
+      .attr("xlink:href", "https://maxcdn.icons8.com/Share/icon/ios7/Files/open_book1600.png")
+      .attr("width", 80)
+      .attr("height", 80)
+
+var laptopImg = laptops.append("svg:image")
+      .attr("xlink:href", "https://cdn.onlinewebfonts.com/svg/img_20446.png")
+      .attr("width", 80)
+      .attr("height", 80)
+
 var text = innerSolar.append("text")
     .data(tucsonData)
-    .attr("x", 310)
-    .attr("y", 500)
+    .attr("x", innerWidth/2 - 55)
+    .attr("y", innerHeight/1.2)
     .style("font-size", "128px")
     .style("font-family", "Reader")
     .style("fill", "#4EB1E9")
