36b7d5e8ff92e898d24e7bd40ff3c81972900f21
annagrace-hc <anna.grace@hackcville.com>
2021-05-12 11:46:41 -0400

    updated tucson

--- tucson-savings/tucson-savings.js
+++ tucson-savings/tucson-savings.js
@@ -59,43 +59,89 @@ var suppliesImg = supplies.append("svg:image")
    .attr("xlink:href", "https://cdn.onlinewebfonts.com/svg/img_532723.png" )
    .attr("width", 80)
    .attr("height", 80)
+var suppliesLabel = supplies.append("text")
+                            .text("School Supplies")
+                            .attr("class", "tusconCatLabel")
+                            .attr("y", 120)
+                            .style("text-anchor", "middle")
+                            .attr("x", 40)
 
 var salImg = salaries.append("svg:image")
    .attr("xlink:href", "http://cdn.onlinewebfonts.com/svg/img_353628.png")
    .attr("width", 80)
    .attr("height", 80)
 
+var salLabel = salaries.append("text")
+                            .text("Teacher Salaries")
+                            .attr("class", "tusconCatLabel")
+                            .attr("y", 120)
+                            .style("text-anchor", "middle")
+                            .attr("x", 40)
+
 var busImg = buses.append("svg:image")
    .attr("xlink:href", "https://cdn0.iconfinder.com/data/icons/transportation-and-logistics/50/Transportation_and_Logistics-67-512.png")
    .attr("width", 80)
    .attr("height", 80)
+
+var busLabel = buses.append("text")
+                            .text("School Buses")
+                            .attr("class", "tusconCatLabel")
+                            .attr("y", 120)
+                            .style("text-anchor", "middle")
+                            .attr("x", 40)
 
 var booksImg = textbooks.append("svg:image")
       .attr("xlink:href", "https://maxcdn.icons8.com/Share/icon/ios7/Files/open_book1600.png")
       .attr("width", 80)
       .attr("height", 80)
 
+var booksLabel = textbooks.append("text")
+                            .text("Textbooks")
+                            .attr("class", "tusconCatLabel")
+                            .attr("y", 120)
+                            .style("text-anchor", "middle")
+                            .attr("x", 40)
+
 var laptopImg = laptops.append("svg:image")
       .attr("xlink:href", "https://cdn.onlinewebfonts.com/svg/img_20446.png")
       .attr("width", 80)
       .attr("height", 80)
 
-var text = innerSolar.append("text")
-    .data(tucsonData)
+var laptopLabel = laptops.append("text")
+                            .text("Laptops")
+                            .attr("class", "tusconCatLabel")
+                            .attr("y", 120)
+                            .style("text-anchor", "middle")
+                            .attr("x", 40)
+
+suppliesImg.on("mouseover", mouseOver(24))
+              .on("mouseout", mouseOut)
+
+let tucsonText = innerSolar.append("text")
+    .attr("class", "tucsonTicker")
     .attr("x", innerWidth/2 - 55)
     .attr("y", innerHeight/1.2)
     .style("font-size", "128px")
     .style("font-family", "Reader")
     .style("fill", "#4EB1E9")
-    .text( function(d) {return d.quantity; } );
+    .text("0");
 
-text.transition()
-  .tween("text", function() {
+
+// on mouseover, transition from zero to amount
+ function mouseOver(totalAmt) {
+     console.log(d3.selectAll(".tucsonTicker"));
+     d3.select(".tucsonTicker").transition().tween("text", function() {
      var selection = d3.select(this); 
-     var start = 1; 
-     var end = d3.select(this).text();                    
+     var start = 0; 
+     var end = totalAmt;  
      var interpolator = d3.interpolateNumber(start,end); 
-     return function(t) { selection.text(Math.round(interpolator(t))); };  // return value
-     
+     return function(t) { selection.text(Math.round(interpolator(t))); };  // return value     
   })
-  .duration(5000);
+  .duration(5000);  
+    }
+
+    function mouseOut() {
+        
+    }
+
+
